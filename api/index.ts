e36756ca59150fbe83bfc5941f45f45e89b27f27
niya-s <niya.somkerd@gmail.com>
2025-07-27 18:50:51 +0700

    fix: delete vercel.json

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { app } from "../src/app";
-import serverlessExpress from "@vendia/serverless-express";
-
-const handler = serverlessExpress({ app });
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  return handler(req, res);
-};
